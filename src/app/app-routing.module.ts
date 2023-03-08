efab012a3a3a229aeede646ca765946bb6f55cb7
diegue-exe <dieeegue2104@gmail.com>
2023-03-08 21:15:38 +0100

    Eliminados componentes innecesarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -74,44 +74,6 @@ import { TypographyComponent } from './bootstrap/typography/typography.component
 import { GridComponent } from './bootstrap/grid/grid.component';
 
 
-
-import { DzmtAutocompleteComponent } from './material/dzmt-autocomplete/dzmt-autocomplete.component';
-import { DzmtBadgeComponent } from './material/dzmt-badge/dzmt-badge.component';
-import { DzmtBottomSheetComponent } from './material/dzmt-bottom-sheet/dzmt-bottom-sheet.component';
-import { DzmtButtonComponent } from './material/dzmt-button/dzmt-button.component';
-import { DzmtButtonToggleComponent } from './material/dzmt-button-toggle/dzmt-button-toggle.component';
-import { DzmtCardComponent } from './material/dzmt-card/dzmt-card.component';
-import { DzmtCheckboxComponent } from './material/dzmt-checkbox/dzmt-checkbox.component';
-import { DzmtChipsComponent } from './material/dzmt-chips/dzmt-chips.component';
-import { DzmtDatepickerComponent } from './material/dzmt-datepicker/dzmt-datepicker.component';
-import { DzmtDialogComponent } from './material/dzmt-dialog/dzmt-dialog.component';
-import { DzmtDividerComponent } from './material/dzmt-divider/dzmt-divider.component';
-import { DzmtExpansionComponent } from './material/dzmt-expansion/dzmt-expansion.component';
-import { DzmtFormFieldComponent } from './material/dzmt-form-field/dzmt-form-field.component';
-import { DzmtGridListComponent } from './material/dzmt-grid-list/dzmt-grid-list.component';
-import { DzmtIconComponent } from './material/dzmt-icon/dzmt-icon.component';
-import { DzmtInputComponent } from './material/dzmt-input/dzmt-input.component';
-import { DzmtListComponent } from './material/dzmt-list/dzmt-list.component';
-import { DzmtMenuComponent } from './material/dzmt-menu/dzmt-menu.component';
-import { DzmtPaginatorComponent } from './material/dzmt-paginator/dzmt-paginator.component';
-import { DzmtProgressBarComponent } from './material/dzmt-progress-bar/dzmt-progress-bar.component';
-import { DzmtProgressSpinnerComponent } from './material/dzmt-progress-spinner/dzmt-progress-spinner.component';
-import { DzmtRadioComponent } from './material/dzmt-radio/dzmt-radio.component';
-import { DzmtRippleComponent } from './material/dzmt-ripple/dzmt-ripple.component';
-import { DzmtSelectComponent } from './material/dzmt-select/dzmt-select.component';
-import { DzmtSidenavComponent } from './material/dzmt-sidenav/dzmt-sidenav.component';
-import { DzmtSlideToggleComponent } from './material/dzmt-slide-toggle/dzmt-slide-toggle.component';
-import { DzmtSliderComponent } from './material/dzmt-slider/dzmt-slider.component';
-import { DzmtSnackBarComponent } from './material/dzmt-snack-bar/dzmt-snack-bar.component';
-import { DzmtSortComponent } from './material/dzmt-sort/dzmt-sort.component';
-import { DzmtStepperComponent } from './material/dzmt-stepper/dzmt-stepper.component';
-import { DzmtTableComponent } from './material/dzmt-table/dzmt-table.component';
-import { DzmtTabsComponent } from './material/dzmt-tabs/dzmt-tabs.component';
-import { DzmtTooltipComponent } from './material/dzmt-tooltip/dzmt-tooltip.component';
-import { DzmtTreeComponent } from './material/dzmt-tree/dzmt-tree.component';
-import { DzmtToolbarComponent } from './material/dzmt-toolbar/dzmt-toolbar.component';
-
-
 import { NestableComponent } from './plugins/nestable/nestable.component';
 import { LightGalleryComponent } from './plugins/light-gallery/light-gallery.component';
 
@@ -218,45 +180,6 @@ const routes: Routes = [
                         {path: 'ui-typeahead', component: TypeaheadComponent},
                         {path: 'ui-typography', component: TypographyComponent},
                         {path: 'ui-grid', component: GridComponent},
-
-
-
-
-                        {path: 'mat-autocomplete', component: DzmtAutocompleteComponent},
-                        {path: 'mat-badge', component: DzmtBadgeComponent},
-                        {path: 'mat-bottom-sheet', component: DzmtBottomSheetComponent},
-                        {path: 'mat-button', component: DzmtButtonComponent},
-                        {path: 'mat-button-toggle', component: DzmtButtonToggleComponent},
-                        {path: 'mat-card', component: DzmtCardComponent},
-                        {path: 'mat-checkbox', component: DzmtCheckboxComponent},
-                        {path: 'mat-chips', component: DzmtChipsComponent},
-                        {path: 'mat-datepicker', component: DzmtDatepickerComponent},
-                        {path: 'mat-dialog', component: DzmtDialogComponent},
-                        {path: 'mat-divider', component: DzmtDividerComponent},
-                        {path: 'mat-expansion', component: DzmtExpansionComponent},
-                        {path: 'mat-form-field', component: DzmtFormFieldComponent},
-                        {path: 'mat-grid-list', component: DzmtGridListComponent},
-                        {path: 'mat-icon', component: DzmtIconComponent},
-                        {path: 'mat-input', component: DzmtInputComponent},
-                        {path: 'mat-list', component: DzmtListComponent},
-                        {path: 'mat-menu', component: DzmtMenuComponent},
-                        {path: 'mat-paginator', component: DzmtPaginatorComponent},
-                        {path: 'mat-progress-bar', component: DzmtProgressBarComponent},
-                        {path: 'mat-progress-spinner', component: DzmtProgressSpinnerComponent},
-                        {path: 'mat-radio', component: DzmtRadioComponent},
-                        {path: 'mat-ripple', component: DzmtRippleComponent},
-                        {path: 'mat-select', component: DzmtSelectComponent},
-                        {path: 'mat-sidenav', component: DzmtSidenavComponent},
-                        {path: 'mat-slide-toggle', component: DzmtSlideToggleComponent},
-                        {path: 'mat-slider', component: DzmtSliderComponent},
-                        {path: 'mat-snack-bar', component: DzmtSnackBarComponent},
-                        {path: 'mat-sort', component: DzmtSortComponent},
-                        {path: 'mat-stepper', component: DzmtStepperComponent},
-                        {path: 'mat-table', component: DzmtTableComponent},
-                        {path: 'mat-tab', component: DzmtTabsComponent},
-                        {path: 'mat-tooltip', component: DzmtTooltipComponent},
-                        {path: 'mat-tree', component: DzmtTreeComponent},
-                        {path: 'mat-toolbar', component: DzmtToolbarComponent},
 
                         {path: 'uc-nestable', component: NestableComponent},
                         {path: 'uc-lightgallery', component: LightGalleryComponent},
